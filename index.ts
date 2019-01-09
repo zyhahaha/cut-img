cc80ac79670627c693f91e0f39060f5332dcbfc1
zhaoyang <18355403288@163.com>
2019-01-09 10:39:58 +0800

    ts type

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // eg:
 // let url = '/api/private_barcode_tmpl_image/upload';
 // let file = {}; // 文件对象
-// var fileData = {
+// let fileData = {
 //   file: file,
 //   max: 140,
 //   url: url
@@ -11,6 +11,13 @@
 // })
 
 import * as EXIF from 'exif-js';
+
+interface IDrawData {
+  file: {
+    type: string
+  },
+  max: number
+}
 /**
  * 裁剪图片
  * @param data = {
@@ -19,13 +26,13 @@ import * as EXIF from 'exif-js';
  * }
  */
 export default class ImgUploadService {
-  constructor(Upload) {
+  constructor() {
     // this.Upload = Upload;
   };
 
   //获取图片方向
-  public getPhotoOrientation(img, next) {
-    var orient = 1;
+  public getPhotoOrientation(img: object, next: Function) {
+    let orient = 1;
     // next(orient);
     EXIF.getData(img, () => {
       orient = EXIF.getTag(this, 'Orientation');
@@ -33,17 +40,35 @@ export default class ImgUploadService {
     });
   }
 
-  draw(data, next, id) {
-    var that = this;
-    var canvas, file = data.file,
-      max = data.max;
-    var context, img;
+  draw(data: IDrawData, next: Function, id: string) {
+    let that = this;
+    let max = data.max;
+    let canvas: {
+      width: number,
+      height: number,
+      toDataURL: Function,
+      setAttribute: Function,
+      getContext: Function
+    };
+    let file = data.file;
+    let context: {
+      clearRect: Function,
+      translate: Function,
+      rotate: Function,
+      drawImage: Function
+    };
+    let img: {
+      src: string | null,
+      onload: Function | null,
+      width: number,
+      height: number
+    };
     if (that.isImage(file.type)) {
       img = new Image();
       img.src = that.getObjectURL(file);
       img.onload = () => {
-        that.getPhotoOrientation(img, orient => {
-          var maxWidth = img.width,
+        that.getPhotoOrientation(img, (orient: number) => {
+          let maxWidth = img.width,
             maxHeight = img.height;
           if (img.width > img.height) {
             if (img.width > max) {
@@ -63,25 +88,25 @@ export default class ImgUploadService {
           }
 
           if (orient === 6) {
-            canvas.setAttribute('width', maxHeight);
-            canvas.setAttribute('height', maxWidth);
+            canvas.setAttribute('width', String(maxHeight));
+            canvas.setAttribute('height', String(maxWidth));
           } else {
-            canvas.setAttribute('width', maxWidth);
-            canvas.setAttribute('height', maxHeight);
+            canvas.setAttribute('width', String(maxWidth));
+            canvas.setAttribute('height', String(maxHeight));
           }
           context = canvas.getContext('2d');
           context.clearRect(0, 0, canvas.width, canvas.height);
           if (orient === 6) {
             // context.save();
-            context.translate(maxHeight, 0);
+            context.translate(String(maxHeight), 0);
             context.rotate(90 * Math.PI / 180);
             context.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.height, canvas.width);
             // context.restore();
           } else {
             context.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
           }
-          var strDataURI = canvas.toDataURL(file.type);
-          var blob = that.dataURItoBlob(strDataURI);
+          let strDataURI = canvas.toDataURL(file.type);
+          let blob = that.dataURItoBlob(strDataURI);
           // data.file = blob;
           next(blob);
         });
@@ -89,7 +114,7 @@ export default class ImgUploadService {
     }
   }
 
-  isImage(type) {
+  isImage(type: string) {
     switch (type) {
       case 'image/jpeg':
       case 'image/png':
@@ -102,8 +127,8 @@ export default class ImgUploadService {
     }
   }
 
-  getObjectURL(file) {
-    var url = null;
+  getObjectURL(file: {type: string}) {
+    let url = null;
     if (URL !== undefined) {
       url = URL.createObjectURL(file);
     } else if (webkitURL !== undefined) {
@@ -112,26 +137,26 @@ export default class ImgUploadService {
     return url;
   };
 
-  dataURItoBlob(dataURI) {
+  dataURItoBlob(dataURI: string) {
     // convert base64 to raw binary data held in a string
     // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-    var byteString = atob(dataURI.split(',')[1]);
+    let byteString = atob(dataURI.split(',')[1]);
 
     // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 
     // write the bytes of the string to an ArrayBuffer
-    var ab = new ArrayBuffer(byteString.length);
-    var ia = new Uint8Array(ab);
-    for (var i = 0; i < byteString.length; i++) {
+    let ab = new ArrayBuffer(byteString.length);
+    let ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
       ia[i] = byteString.charCodeAt(i);
     }
     // write the ArrayBuffer to a blob, and you're done
-    var blob = new Blob([ab], { type: mimeString });
+    let blob = new Blob([ab], { type: mimeString });
     return blob;
 
     // Old code
-    // var bb = new BlobBuilder();
+    // let bb = new BlobBuilder();
     // bb.append(ab);
     // return bb.getBlob(mimeString);
   }
